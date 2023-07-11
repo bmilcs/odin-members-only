6e624cc345d06b37d58823527106ce38e29f1095
Bryan Miller <bmilcs@yahoo.com>
2023-07-11 16:36:51 -0400

    Add basic routes to routes/index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// homepage route
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
+
+// signup
+router
+  .route("/signup")
+  .get((req, res) => {
+    res.render("signup");
+  })
+  .post((req, res) => {
+    res.send(req.body);
+  });
+
+// login
+router
+  .route("/login")
+  .get((req, res) => {
+    res.render("login");
+  })
+  .post((req, res) => {
+    res.send(req.body);
+  });
+
+// logout
+router.get("/logout", (req, res) => {
+  res.send("logout route");
+});
+
+// messages
+router
+  .route("/messages")
+  .get((req, res) => {
+    res.send("messages route");
+  })
+  .post((req, res) => {
+    res.send(req.body);
+  });
 
 module.exports = router;
