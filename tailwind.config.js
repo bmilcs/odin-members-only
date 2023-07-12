9397345b36ce4a38755103f051a946d3a550707e
Bryan Miller <bmilcs@yahoo.com>
2023-07-12 18:59:30 -0400

    Add pretter w/ tailwind plugin packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/**/*.ejs"],
+  content: ["./views/*.ejs", "/views/partials/*.ejs"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ["Open Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
