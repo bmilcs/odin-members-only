7e4a690304eb6dd292ff8738d074922bcfcc43c1
Bryan Miller <bmilcs@yahoo.com>
2023-07-12 19:02:17 -0400

    Remove users route & add auth package imports > app.js

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 
 const app = express();
 
@@ -24,12 +26,15 @@ async function main() {
 }
 
 //
-// express default middleware
+// view engine setup
 //
 
-// view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+//
+// authentication: passport middleware
+//
 
 // express-generator middleware
 app.use(logger("dev"));
@@ -43,7 +48,6 @@ app.use(express.static(path.join(__dirname, "public")));
 //
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 //
 // error handling middleware
